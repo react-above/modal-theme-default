d3a83e4e22b022a7877ba46093fec2f236b7de88
Evgeny Zakharov <risenforces@gmail.com>
2022-05-03 22:26:55 +0300

    chore: update demo

--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,63 @@
-import React, { FC, useState } from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 import {
-  configure,
-  FocusLockPlugin,
-  Modal,
+  createModal,
   ScrollLockPlugin,
+  FocusLockPlugin,
 } from '@react-above/modal'
 import { ThemeDefault } from '@react-above/modal-theme-default'
 import '@react-above/modal-theme-default/styles.scss'
 
-configure({
+const Modal = createModal({
   theme: ThemeDefault(),
   plugins: [ScrollLockPlugin(), FocusLockPlugin()],
 })
 
-const App: FC = () => {
+const ModalWithChildren = () => {
   const [isOpen, setOpen] = useState(true)
   const open = () => setOpen(true)
   const close = () => setOpen(false)
 
   return (
     <div>
-      <h1>@react-above/modal-theme-default</h1>
-      <button onClick={open}>Open modal</button>
-
+      <button onClick={open}>Open modal with "children"</button>
       <Modal isOpen={isOpen} close={close}>
         <Modal.Surface>
           <Modal.Header title="My modal" close={close} />
-          <Modal.Body>Hello world</Modal.Body>
+          <Modal.Body>My modal description</Modal.Body>
         </Modal.Surface>
       </Modal>
     </div>
   )
 }
+
+const ModalWithRender = () => {
+  const [isOpen, setOpen] = useState(false)
+  const open = () => setOpen(true)
+  const close = () => setOpen(false)
+
+  return (
+    <div>
+      <button onClick={open}>Open modal with "render"</button>
+      <Modal
+        isOpen={isOpen}
+        close={close}
+        render={({ modalRef, close }) => (
+          <Modal.Surface ref={modalRef}>
+            <Modal.Header title="My modal" close={close} />
+            <Modal.Body>My modal description</Modal.Body>
+          </Modal.Surface>
+        )}
+      />
+    </div>
+  )
+}
+
+const App = () => (
+  <>
+    <ModalWithChildren />
+    <ModalWithRender />
+  </>
+)
 
 render(<App />, document.querySelector('#root'))
